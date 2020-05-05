fe643a566d95a9d5607eb5a80ff24ed18dc3c8a8
munhozfontana <munhozfontana@gmail.com>
2020-05-04 21:12:33 -0300

    move sut creation to a factory helper method

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -5,16 +5,20 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('munhozfontana@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return fasle if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_emailmail.com')
     expect(isEmailValid).toBe(false)
   })
